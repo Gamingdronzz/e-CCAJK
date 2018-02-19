059b9b160009a2401c85afecdd1e9230ead91f8d
Balpreet Singh <balpreets10@gmail.com>
2018-02-19 15:42:59 +0530

    Contact Us Finished

--- app/src/main/java/com/ccajk/Fragments/ContactUsFragment.java
+++ app/src/main/java/com/ccajk/Fragments/ContactUsFragment.java
@@ -46,14 +46,20 @@ public class ContactUsFragment extends Fragment {
     private void getContactsList() {
         contactArrayList.add(new ContactBuilder().setName("Sh. Rajnish Kumar Jenaw ").setDesignation("CCA\nccajk@nic.in").setOfficeContact("2477280").setMobileContact("9419120080").createContact());
         contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
-        contactArrayList.add(new ContactBuilder().setName("Sh Amanullah Tak").setDesignation("JT.CCA\naman.tak@gov.in").setOfficeContact("2477281").setMobileContact("9419120986").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Smt. Veena Gupta").setDesignation("ACCA (Spectrum/Pension)\naccajk@nic.in").setOfficeContact("2477283").setMobileContact("9419120332").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh Jagdish Bunkar").setDesignation("ACCA (USO,LF I)").setOfficeContact("2477284").setMobileContact("9858431983").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh. Kartar Nath").setDesignation("AO (Admin,Cash/GPF)\ncao.adminjk@nic.in").setOfficeContact("2475858").setMobileContact("9419120984").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh Ram Asra").setDesignation("AAO(Pension)").setOfficeContact("2477285").setMobileContact("9855502925").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh Sanjeev Kumar").setDesignation("AAO(Cash/GPF)").setOfficeContact("2477283").setMobileContact("9469503844").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh. Vinod Pandita").setDesignation("Cashier").setOfficeContact("2479268").setMobileContact("9419178910").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh. Sardaroo Ram").setDesignation("Diary & Despatch").setOfficeContact("2477283").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Smt. Barkha Dhar").setDesignation("USO").setOfficeContact("2479548").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh. M.L. Sharma").setDesignation("Consultant(Spectrum)").setOfficeContact("2477285").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh. R.S Sharma ").setDesignation("Consultant(LF)").setOfficeContact("2477282").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh. D.D Sharma").setDesignation("Consultant(LF)").setOfficeContact("2477282").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh. Ram Lal").setDesignation("Consultant(Admin)").setOfficeContact("2477282").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Smt. Nirmal Sharma").setDesignation("Consultant(Pen)").setOfficeContact("2477284").createContact());
+        contactArrayList.add(new ContactBuilder().setName("Sh Neeraj Koul").setDesignation("Consultant (Pension)").setOfficeContact("2477284").setMobileContact("9419286585").createContact());
     }
 
 }
