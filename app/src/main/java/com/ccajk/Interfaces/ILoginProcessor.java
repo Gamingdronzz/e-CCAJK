7d3e07992e854509bf4de88f149690e451ecdd48
Balpreet Singh <balpreets10@gmail.com>
2018-02-27 11:48:30 +0530

    Login

---
+++ app/src/main/java/com/ccajk/Interfaces/ILoginProcessor.java
@@ -0,0 +1,13 @@
+package com.ccajk.Interfaces;
+
+/**
+ * Created by balpreet on 2/27/2018.
+ */
+
+public interface ILoginProcessor {
+
+    void RequestLogin();
+    void OnLoginSuccesful();
+    void OnLoginFailure();
+
+}
