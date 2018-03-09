a2b688787088d42d691133de06c6c46a0a2939e6
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2018-03-09 14:00:42 +0530

    Design Changes

--- app/src/main/java/com/ccajk/Fragments/InspectionFragment.java
+++ app/src/main/java/com/ccajk/Fragments/InspectionFragment.java
@@ -1,8 +1,10 @@
 package com.ccajk.Fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.content.res.AppCompatResources;
 import android.util.Log;
@@ -11,15 +13,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.ccajk.R;
-
-import java.io.File;
-import java.util.List;
-
-import easyfilepickerdialog.kingfisher.com.library.model.DialogConfig;
-import easyfilepickerdialog.kingfisher.com.library.model.SupportFile;
-import easyfilepickerdialog.kingfisher.com.library.view.FilePickerDialogFragment;
+import com.linchaolong.android.imagepicker.ImagePicker;
+import com.linchaolong.android.imagepicker.cropper.CropImage;
+import com.linchaolong.android.imagepicker.cropper.CropImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,7 +27,9 @@ public class InspectionFragment extends Fragment {
 
     private static final String TAG = "Inspection";
     ImageButton choose, location;
+    TextView textChoose, textLocation;
     Button upload;
+    ImagePicker imagePicker;
 
     public InspectionFragment() {
 
@@ -44,14 +45,23 @@ public class InspectionFragment extends Fragment {
     }
 
     private void init(View view) {
+        textChoose=view.findViewById(R.id.textview_choose);
+        textChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImageChooser();
+            }
+        });
         choose = view.findViewById(R.id.button_choose);
         choose.setImageDrawable(AppCompatResources.getDrawable(getContext(), R.drawable.ic_add_circle_black_24dp));
         choose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showFilePicker();
+                showImageChooser();
             }
         });
+
+        textLocation=view.findViewById(R.id.textview_location);
 
         location = view.findViewById(R.id.button_location);
         location.setImageDrawable(AppCompatResources.getDrawable(getContext(), R.drawable.ic_add_location_black_24dp));
@@ -61,31 +71,52 @@ public class InspectionFragment extends Fragment {
 
     }
 
-    private void showFilePicker() {
+    private void showImageChooser() {
 
-        DialogConfig dialogConfig = new DialogConfig.Builder()
-                .enableMultipleSelect(false) // default is false
-                .enableFolderSelect(false) // default is false
-                .initialDirectory(Environment.getExternalStorageDirectory().getAbsolutePath()) // default is sdcard
-                .supportFiles( new SupportFile(".jpg", 0),new SupportFile(".png", 0), new SupportFile(".pdf", 0)) // default is showing all file types.
-                .build();
+        imagePicker = new ImagePicker();
+        imagePicker.startChooser(this, new ImagePicker.Callback() {
+            @Override
+            public void onPickImage(Uri imageUri) {
+                Log.d(TAG, "onPickImage: " + imageUri.getPath());
+            }
 
-        new FilePickerDialogFragment.Builder()
-                .configs(dialogConfig)
-                .onFilesSelected(new FilePickerDialogFragment.OnFilesSelectedListener() {
-                    @Override
-                    public void onFileSelected(List<File> list) {
-                        Log.e(TAG, "total Selected file: " + list.size());
-                        for (File file : list) {
-                            Log.e(TAG, "Selected file: " + file.getAbsolutePath());
-                        }
-                    }
-                })
-                .build()
-                .show(getChildFragmentManager(), null);
+            @Override
+            public void onCropImage(Uri imageUri) {
+                Log.d(TAG, "onCropImage: " + imageUri.getPath());
+                //draweeView.setImageURI(imageUri);
+                // draweeView.getHierarchy().setRoundingParams(RoundingParams.asCircle());
+            }
+
+            @Override
+            public void cropConfig(CropImage.ActivityBuilder builder) {
+                builder
+                        .setMultiTouchEnabled(false)
+                        .setGuidelines(CropImageView.Guidelines.OFF)
+                        .setCropShape(CropImageView.CropShape.RECTANGLE)
+                        .setRequestedSize(960, 540)
+                        .setAspectRatio(16, 9);
+            }
+
+            @Override
+            public void onPermissionDenied(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+            }
+        });
 
 
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imagePicker.onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        imagePicker.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
     }
 
 }
