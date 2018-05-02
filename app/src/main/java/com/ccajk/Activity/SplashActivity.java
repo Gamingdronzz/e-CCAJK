3cf6ed07bdc09ec6f9347877e8bc9f5d264f0b15
Balpreet Singh <balpreet.s@luckystrikestudios.com>
2018-05-02 15:29:16 +0530

    Change Nearby Layout

--- app/src/main/java/com/ccajk/Activity/SplashActivity.java
+++ app/src/main/java/com/ccajk/Activity/SplashActivity.java
@@ -36,8 +36,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        bindVIews();
         Helper.getInstance().setDebugMode(true);
+        bindVIews();
+
         //checkForUpdate();
         StartAnimations();
     }
