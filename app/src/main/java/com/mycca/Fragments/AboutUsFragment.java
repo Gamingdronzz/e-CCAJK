2ef60e042bcde1fe8483f7f651d0148ce0b59bbe
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2018-06-04 22:05:15 +0530

    About us

--- app/src/main/java/com/mycca/Fragments/AboutUsFragment.java
+++ app/src/main/java/com/mycca/Fragments/AboutUsFragment.java
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 
 import com.mycca.Activity.MainActivity;
 import com.mycca.R;
@@ -15,21 +15,21 @@ import com.mycca.R;
  */
 
 public class AboutUsFragment extends Fragment {
-    LinearLayout linearLayoutLogo;
+    ImageView imageViewLogo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about_us, container, false);
-        linearLayoutLogo = view.findViewById(R.id.layout_logo);
-        linearLayoutLogo.setOnClickListener(new View.OnClickListener() {
+        imageViewLogo = view.findViewById(R.id.img_about_logo);
+        imageViewLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MainActivity mainActivity = (MainActivity) getActivity();
                 BrowserFragment fragment = new BrowserFragment();
                 Bundle bundle = new Bundle();
                 bundle.putString("url", "http://gamingdronzz.com");
-                mainActivity.ShowFragment("Gaming Dronzz",fragment,bundle);
+                mainActivity.ShowFragment("Gaming Dronzz", fragment, bundle);
 
             }
         });
